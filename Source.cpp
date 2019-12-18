147e7d277a2e1f1b3d04dcc70f1b6b702b345ba4
AARON M <asmarcks@gmail.com>
2019-12-18 01:25:02 -0600

    calculating distance to walls and player ray

--- Source.cpp
+++ Source.cpp
@@ -12,7 +12,7 @@ int nMapHeight = 16;
 int nMapWidth = 16;
 
 float fFOV = 3.14159 / 4.0;
-
+float fDepth = 16.0f;
 int main()
 {
     //create screen buffer//
@@ -46,11 +46,57 @@ int main()
         for (int x = 0; x < nScreenWidth; x++)
             
         {
+            //calculate projected ray angle into world space//
             float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float) x / (float)nScreenWidth) * fFOV;
-        }
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
 
+            //unit vector for ray in player space//
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            while(!bHitWall && fDistanceToWall < fDepth);
+            {
+                fDistanceToWall += 0.1f;
+
+                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+                //test if ray is out of bounds//
+                if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+                {
+                    //ray distance set to maximum depth//
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                }
+                else
+                {
+                    if(map[nTestY * nMapWidth + nTestX] == '#')
+                    {
+                        bHitWall = true;
+                    }
+                }
+            }
+            //distance to ceiling and floor//
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float) fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+
+            for (int y = 0; y < nScreenHeight; y++)
+            {
+                if(y < nCeiling)
+                    screen[y*nScreenWidth + x] = ' ';
+                else if(y > nCeiling && y <= nFloor)
+                    screen[y*nScreenWidth + x] = '#';
+                else
+                    screen[y*nScreenWidth + x] = ' ';
+                    
+
+            }
+        }
+    
         screen[nScreenWidth * nScreenHeight - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, {0, 0}, &dwBytesWritten);
+
     }
     return 0;
 }
